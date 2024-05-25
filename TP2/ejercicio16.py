7298b68516c1c1cb044b5bee98b11195ccb98950
seba <vignesebastian2@gmail.com>
2024-05-24 22:59:29 -0300

    arreglo ejercicio 16 tp2

--- TP2/ejercicio16.py
+++ TP2/ejercicio16.py
@@ -17,15 +17,19 @@ def interseccion_de_pilas(pila1,pila2):
 
 episodio_V = Stack()
 for i in range(5):
-    Personajes_EpV = input(print("Ingrese personajes",i+1,"del Episodio V: "))
+    Personajes_EpV = input("Ingrese personajes del Episodio V: ")
     episodio_V.push(Personajes_EpV)
+
+print()
     
 episodio_VII = Stack()
 for i in range(5):
-    Personajes_EpVII = input(print("Ingrese personajes",i+1,"del Episodio VII: " ))
+    Personajes_EpVII = input("Ingrese personajes del Episodio VII: " )
     episodio_VII.push(Personajes_EpVII)
     
 interseccion = interseccion_de_pilas(episodio_V, episodio_VII)
+
+print()
 
 print("Los personajes que aparecen en ambos episodios son los siguientes:")
 while not interseccion.size() == 0:
